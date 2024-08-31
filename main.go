22bd2351a77a58d5fe80f178a21d7f0888a0b909
gitgerby <gearnsc@gmail.com>
2024-08-31 12:59:08 -0400

    Enable power throttling / EcoQOS

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ import (
 	"path/filepath"
 	"sync"
 	"time"
+	"unsafe"
 
 	"database/sql"
 
@@ -393,6 +394,12 @@ func enqueueCopy(tj *TranscodeJob) {
 	queueCopy = append(queueCopy, tj)
 }
 
+type PROCESS_POWER_THROTTLING_STATE struct {
+	Version     uint32
+	ControlMask uint32
+	StateMask   uint32
+}
+
 // lowerPriority sets the process to the lowest scheduler priority
 func lowerPriority() error {
 	ph, err := windows.OpenProcess(windows.PROCESS_QUERY_LIMITED_INFORMATION|windows.PROCESS_SET_INFORMATION, false, uint32(os.Getpid()))
@@ -403,7 +410,11 @@ func lowerPriority() error {
 	if err != nil {
 		return fmt.Errorf("windows.SetPriorityClass for pid: %v returned: %v", uint32(os.Getpid()), err)
 	}
-	return nil
+
+	// Ecoqos / ecomode / power state throttling
+	t := PROCESS_POWER_THROTTLING_STATE{1, 1, 1}
+	return windows.NtSetInformationProcess(ph, 77, unsafe.Pointer(&t), uint32(unsafe.Sizeof(t)))
+
 }
 
 func main() {
