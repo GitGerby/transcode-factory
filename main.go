96397062f10acfb5a9a6d74626546662c5189e3c
gitgerby <gearnsc@gmail.com>
2024-10-19 12:17:33 -0400

    split out OS specific calls and support building in multiple environments

--- main.go
+++ main.go
@@ -25,14 +25,12 @@ import (
 	"path/filepath"
 	"sync"
 	"time"
-	"unsafe"
 
 	"database/sql"
 
 	"github.com/google/logger"
 	"github.com/kardianos/service"
 	"golang.org/x/sync/errgroup"
-	"golang.org/x/sys/windows"
 	_ "modernc.org/sqlite"
 )
 
@@ -392,29 +390,6 @@ func enqueueCopy(tj *TranscodeJob) {
 	defer func() { muCopy.Unlock() }()
 	updateJobStatus(tj.Id, AwaitingTranscode)
 	queueCopy = append(queueCopy, tj)
-}
-
-type PROCESS_POWER_THROTTLING_STATE struct {
-	Version     uint32
-	ControlMask uint32
-	StateMask   uint32
-}
-
-// lowerPriority sets the process to the lowest scheduler priority
-func lowerPriority() error {
-	ph, err := windows.OpenProcess(windows.PROCESS_QUERY_LIMITED_INFORMATION|windows.PROCESS_SET_INFORMATION, false, uint32(os.Getpid()))
-	if err != nil {
-		return fmt.Errorf("windows.OpenProcess for pid: %v returned: %v", uint32(os.Getpid()), err)
-	}
-	err = windows.SetPriorityClass(ph, windows.IDLE_PRIORITY_CLASS)
-	if err != nil {
-		return fmt.Errorf("windows.SetPriorityClass for pid: %v returned: %v", uint32(os.Getpid()), err)
-	}
-
-	// Ecoqos / ecomode / power state throttling
-	t := PROCESS_POWER_THROTTLING_STATE{1, 1, 1}
-	return windows.NtSetInformationProcess(ph, 77, unsafe.Pointer(&t), uint32(unsafe.Sizeof(t)))
-
 }
 
 func main() {
