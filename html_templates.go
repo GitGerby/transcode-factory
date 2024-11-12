96beb46fe353ed8cbae829c09d436a43e77251ec
gitgerby <gearnsc@gmail.com>
2024-11-11 23:01:56 -0500

    improve status page rendering and visual job differntiation when multiple active jobs are present.

--- html_templates.go
+++ html_templates.go
@@ -4,18 +4,12 @@ const html_template = `
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="refresh" content="300">
+<meta http-equiv="refresh" content="600">
 <style>
-	table, td, th {
-		border: 1px solid;
-	}
-
-	table {
-		width: 100%;
-		border-collapse: collapse;
-	}
-}
-
+  table {width: 100%; border-collapse: collapse;}
+  td, th {border: 1px solid;}
+	.blank_row tr, .blank_row td {border:none;}
+  .highlight_job:hover {background-color: coral;}
 </style>
 </head>
 <body>
@@ -23,42 +17,47 @@ const html_template = `
 	Currently running jobs: {{len .ActiveJobs}}
 	<table>
 		{{range .ActiveJobs}}
-		<tr>
-			<th style="text-align:left">Job ID:</th>
-			<td>{{.Id}}</td>
-			<th style="text-align:left">Stage:</th>
-			<td>{{.State}}</td>
-		</tr>
-		<tr>
-			<th style="text-align:left">Source:</th>
-			<td>{{.JobDefinition.Source}}</td>
-			<th style="text-align:left">Codec:</th>
-			<td>{{.SourceMeta.Codec}}</td>
-		</tr>
-		<tr>
-			<th style="text-align:left">Destination:</th>
-			<td>{{.JobDefinition.Destination}}</td>
-			<th style="text-align:left">Codec / Crf:</th>
-			<td>{{.JobDefinition.Codec}} / {{.JobDefinition.Crf}}</td>
-		</tr>
-		<tr>
-			<th style="text-align:left">Subtitles:</th>
-			<td colspan = "0">
-				<ol>
-					{{range .JobDefinition.Srt_files}}
-						<li>{{.}}</li>
-					{{end}}
-				</ol>
-			</td>
-			<th style="text-align:left">Video Filter:</th>
-			<td>{{.JobDefinition.Video_filters}}</td>
-		</tr>
-		<tr>
-			<th style="text-align:left">Log Output:</th>
-			<td id="log-{{.Id}}"></td>
-			<th style="text-align:left">Duration:</th>
-			<td>{{.SourceMeta.Duration}}</td>
-		</tr>
+		<tbody class="highlight_job">
+				<tr>
+					<th style="text-align:left">Job ID:</th>
+					<td>{{.Id}}</td>
+					<th style="text-align:left">Stage:</th>
+					<td>{{.State}}</td>
+				</tr>
+				<tr>
+					<th style="text-align:left">Source:</th>
+					<td>{{.JobDefinition.Source}}</td>
+					<th style="text-align:left">Codec:</th>
+					<td>{{.SourceMeta.Codec}}</td>
+				</tr>
+				<tr>
+					<th style="text-align:left">Destination:</th>
+					<td>{{.JobDefinition.Destination}}</td>
+					<th style="text-align:left">Codec / Crf:</th>
+					<td>{{.JobDefinition.Codec}} / {{.JobDefinition.Crf}}</td>
+				</tr>
+				<tr>
+					<th style="text-align:left">Subtitles:</th>
+					<td colspan = "0">
+						<ol>
+							{{range .JobDefinition.Srt_files}}
+								<li>{{.}}</li>
+							{{end}}
+						</ol>
+					</td>
+					<th style="text-align:left">Video Filter:</th>
+					<td>{{.JobDefinition.Video_filters}}</td>
+				</tr>
+				<tr>
+					<th style="text-align:left">Log Output:</th>
+					<td id="log-{{.Id}}"></td>
+					<th style="text-align:left">Duration:</th>
+					<td>{{.SourceMeta.Duration}}</td>
+				</tr>
+		</tbody>
+		<tbody>
+        <tr class="blank_row"><td colspan="4" style="height: 1em;"></td></tr>
+		</tbody>
 		{{end}}
 	</table>
 	<h2>Current Queue</h2><br>
