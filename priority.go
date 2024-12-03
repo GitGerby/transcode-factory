ff7352faada3435ff13e173d07d9ab4c23d22471
gitgerby <gearnsc@gmail.com>
2024-12-02 22:24:03 -0500

    I should really test this locally rather than relying on github actions

--- priority.go
+++ priority.go
@@ -11,7 +11,7 @@ import (
 
 func lowerPriority() error {
 	niceValue := 10 // This is a low priority setting (range is -20 to 19 where -20 is the most favorably scheduled)
-	err := syscall.Setpriority(os.Getpid(), syscall.PRIO_PROCESS, niceValue)
+	err := syscall.Setpriority(syscall.PRIO_PROCESS, os.Getpid(), niceValue)
 	if err != nil {
 		return fmt.Errorf("Setpriority for pid: %v returned: %v", os.Getpid(), err)
 	}
