c4b98eb6fafeb2c195d6b3b7e4402896dcd2eeb1
gitgerby <gearnsc@gmail.com>
2024-12-02 22:19:02 -0500

    add test coverage for priority changes

---
+++ priority_test.go
@@ -0,0 +1,39 @@
+//go:build linux || darwin
+// +build linux darwin
+
+package main
+
+import (
+	"os"
+	"syscall"
+	"testing"
+)
+
+func TestLowerPriority(t *testing.T) error {
+	oldPriority, err := syscall.Getpriority(syscall.PRIO_PROCESS, os.Getpid())
+	if err != nil {
+		t.Fatalf("failed to determine current priority of: %v", os.Getpid())
+	}
+
+	t.Cleanup(func() {
+		syscall.Setpriority(syscall.PRIO_PROCESS, os.Getpid(), oldPriority)
+	})
+
+	err = lowerPriority()
+	if err != nil {
+		t.Errorf("failed to lower priority: %v", err)
+	}
+	newPriority, err := syscall.Getpriority(syscall.PRIO_PROCESS, os.Getpid())
+	if err != nil {
+		t.Fatalf("failed to determine new priority of: %v", os.Getpid())
+	}
+	// lowerPriority should have set the priority to 10
+	if newPriority != 10 {
+		t.Errorf("priority after lowering is %d, expected 10", newPriority)
+	}
+	// If the priority was unchanged, log that the test is indeterminate.
+	if oldPriority == newPriority {
+		t.Log("lowerPriority did not change the priority")
+	}
+	return nil
+}
---
+++ priority_windows_test.go
@@ -0,0 +1,50 @@
+//go:build windows
+// +build windows
+
+package main
+
+import (
+	"os"
+
+	"testing"
+
+	"golang.org/x/sys/windows"
+)
+
+func TestLowerPriority(t *testing.T) {
+	ph, err := windows.OpenProcess(windows.PROCESS_QUERY_LIMITED_INFORMATION|windows.PROCESS_SET_INFORMATION, false, uint32(os.Getpid()))
+	if err != nil {
+		t.Errorf("OpenProcess failed: %v", err)
+	}
+
+	oldPriority, err := windows.GetPriorityClass(ph)
+	if err != nil {
+		t.Errorf("GetPriorityClass failed: %v", err)
+	}
+
+	t.Cleanup(func() {
+		err := windows.SetPriorityClass(ph, oldPriority)
+		if err != nil {
+			t.Errorf("returning priority after test failed: %v", err)
+		}
+
+		err = windows.CloseHandle(ph)
+		if err != nil {
+			t.Errorf("closing handle after test failed: %v", err)
+		}
+	})
+
+	err = lowerPriority()
+	if err != nil {
+		t.Errorf("lowerPriority failed: %v", err)
+	}
+
+	newPriority, err := windows.GetPriorityClass(ph)
+	if err != nil {
+		t.Errorf("GetPriorityClass failed: %v", err)
+	}
+
+	if newPriority != windows.IDLE_PRIORITY_CLASS {
+		t.Errorf("priority after test failed: got %d, want %d", newPriority, windows.IDLE_PRIORITY_CLASS)
+	}
+}
