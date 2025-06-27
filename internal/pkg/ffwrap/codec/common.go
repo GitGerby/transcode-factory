2311cebe0e93a02bc77532aa8ef141ba724d904a
gitgerby <gearnsc@gmail.com>
2025-06-26 23:10:15 -0400

    condense types into common in codec pkg

--- internal/pkg/ffwrap/codec/common.go
+++ internal/pkg/ffwrap/codec/common.go
@@ -2,6 +2,38 @@ package codec
 
 import "strings"
 
+const (
+	SideDataTypeMastering  = "Mastering display metadata"
+	SideDataTypeLightLevel = "Content light level metadata"
+)
+
+type ColorCoords struct {
+	Coordinates string
+}
+
+type ColorInfo struct {
+	Color_space     string          `json:"color_space"`
+	Color_primaries string          `json:"color_primaries"`
+	Color_transfer  string          `json:"color_transfer"`
+	Side_data_list  []ColorSideInfo `json:"side_data_list"`
+}
+
+type ColorSideInfo struct {
+	Side_data_type string `json:"side_data_type"`
+	Red_x          string `json:"red_x"`
+	Red_y          string `json:"red_y"`
+	Green_x        string `json:"green_x"`
+	Green_y        string `json:"green_y"`
+	Blue_x         string `json:"Blue_x"`
+	Blue_y         string `json:"Blue_y"`
+	White_point_x  string `json:"White_point_x"`
+	White_point_y  string `json:"White_point_y"`
+	Min_luminance  string `json:"Min_luminance"`
+	Max_luminance  string `json:"Max_luminance"`
+	Max_content    int    `json:"Max_content"`
+	Max_average    int    `json:"Max_average"`
+}
+
 // BuildCodec generates command line arguments for ffmpeg based on the specified codec type and CRF value, with optional color metadata.
 // It supports various codecs including libx265, libsvtav1, hevc_nvenc, and can handle specific configurations based on the provided CRF value and color metadata.
 // The function will always return a valid set of ffmpeg flags for a given codec, if an unrecognized codec is passed then a default libx265 arg slice will be built and returned.
